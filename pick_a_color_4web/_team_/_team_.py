40f6c9205142a8e40d5d7769e0e31a0d9f961c5f
CHENJINGWEN1106 <745829856@qq.com>
2017-06-12 18:33:24 +0800

    Create _team_.py

--- pick_a_color_4web/_team_/_team_.py
+++ pick_a_color_4web/_team_/_team_.py
@@ -1,4 +1,7 @@
-# -*- coding: utf-8 -*-
+reader = csv.DictReader(f)
+
+    print ('\t'.join(reader.fieldnames))
+    for r in reader:# -*- coding: utf-8 -*-
 import csv
 with open('_team_.tsv', encoding='utf8') as f:
     
