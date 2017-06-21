47e92d3c35b96ed63f81829fa22e7bd55e3ab42c
a774032767 <774032767@qq.com>
2017-06-21 17:45:23 +0800

    Add files via upload

---
+++ 123/app/_team_/_team_.py
@@ -0,0 +1,8 @@
+reader = csv.DictReader(f)
+
+    print ('\t'.join(reader.fieldnames))
+    for r in reader:# -*- coding: utf-8 -*-
+import csv
+with open('_team_.tsv', encoding='utf8') as f:
+    
+        print ('\t'.join([r[k] for k in reader.fieldnames]) )
