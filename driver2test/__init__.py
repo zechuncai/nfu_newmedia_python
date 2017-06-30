4bdf9c0b336c95b7320c70ea33fe986e40c72aeb
a774032767 <774032767@qq.com>
2017-06-30 13:46:50 +0800

    f

--- driver2test/__init__.py
+++ driver2test/__init__.py
@@ -1,3 +1,3 @@
 from flask import Flask,render_template
 app = Flask(__name__) 
-from app import views
+from driver2test import views
--- run.py
+++ run.py
@@ -1,2 +1,2 @@
-from app import app
-app.run(debug = True)
+from driver2test import app
+app.run(debug=True)
