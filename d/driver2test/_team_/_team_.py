9417e76bcc13ea80434be4e3e15e6d71939d9473
Cailiangz <807844741@qq.com>
2017-07-18 23:23:16 +0800

    Update _team_.py

--- d/driver2test/_team_/_team_.py
+++ d/driver2test/_team_/_team_.py
@@ -6,3 +6,8 @@ import csv
 with open('_team_.tsv', encoding='utf8') as f:
     
         print ('\t'.join([r[k] for k in reader.fieldnames]) )
+<html>
+<body>
+        <body bgcolor="#FF0000">
+    </body>
+</html>
